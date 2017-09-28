04faaf2c3945992fb5425e488b5439513e23618d
mzapeka <mzapeka@gmail.com>
2017-09-29 01:24:04 +0300

    исправлена проблемма с URL менеджером

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -46,9 +46,9 @@ return [
             'errorAction' => 'site/error',
         ],
 
-        'backendUrlManager' => require __DIR__ . '/../../backend/config/urlManager.php',
         'frontendUrlManager' => require __DIR__ . '/urlManager.php',
-        'urlManager' => function () {
+        'backendUrlManager' => require __DIR__ . '/../../backend/config/urlManager.php',
+        'urlManager' => function(){
             return Yii::$app->get('frontendUrlManager');
         },
 
--- frontend/config/urlManager.php
+++ frontend/config/urlManager.php
@@ -29,16 +29,16 @@ return [
         'blog/<slug:[\w\-]+>' => 'blog/post/category',
 
         'catalog' => 'shop/catalog/index',
-        ['class' => 'frontend\urls\CategoryUrlRule'],
+//        ['class' => 'frontend\urls\CategoryUrlRule'],
         'catalog/<id:\d+>' => 'shop/catalog/product',
 
         'cabinet' => 'cabinet/default/index',
         'cabinet/<_c:[\w\-]+>' => 'cabinet/<_c>/index',
-        'cabinet/<_c:[\w\-]+>/<id:\d+>' => 'cabinet/<_c>/view',
+       'cabinet/<_c:[\w\-]+>/<id:\d+>' => 'cabinet/<_c>/view',
         'cabinet/<_c:[\w\-]+>/<_a:[\w-]+>' => 'cabinet/<_c>/<_a>',
         'cabinet/<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => 'cabinet/<_c>/<_a>',
 
-        ['class' => 'frontend\urls\PageUrlRule'],
+//        ['class' => 'frontend\urls\PageUrlRule'],
 
         '<_c:[\w\-]+>' => '<_c>/index',
         '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
